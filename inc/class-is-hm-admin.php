a93eef084651383703c14c552bca0d9e2bbb94d1
Dan Westall <dan.westall@googlemail.com>
2017-05-15 17:08:35 +0100

    Update to cs standards

--- inc/class-is-hm-admin.php
+++ inc/class-is-hm-admin.php
@@ -1,32 +1,6 @@
 <?php
-/**
- * Plugin Name: is_hm_admin
- * Description: Adds a custom capability and some helper functions to determine if the current user is a privileged Human Made or Standard Chartered user.
- * Version:     1.3.0
- * Author:      Human Made
- * Author URI:  http://hmn.md
- * License:     GPLv2
- *
- * @package is_hm_admin
- */
 
-/**
- * Copyright (c) 2016 Human Made
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2 or, at
- * your discretion, any later version, as published by the Free
- * Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
+namespace HM\Is_Admin;
 
 /**
  * Main initiation class
@@ -61,7 +35,7 @@ class Is_HM_Admin {
 	 * @var    array
 	 * @since  1.0.0
 	 */
-	public $privileged_users = array();
+	public $privileged_users = [];
 
 	/**
 	 * Singleton instance of plugin
@@ -248,28 +222,3 @@ class Is_HM_Admin {
 		}
 	}
 }
-
-/**
- * Grab the Is_HM_Admin object and return it.
- * Wrapper for Is_HM_Admin::get_instance()
- *
- * @since  1.0.0
- * @return Is_HM_Admin  Singleton instance of plugin class.
- */
-function hm_is_admin() {
-	return Is_HM_Admin::get_instance();
-}
-
-/**
- * Checks if the current user is hm_admin and has special capabilities.
- *
- * @param  int|object $user         A specific user to check. Can be the WP_User object or an ID.
- * @param  boolean    $bypass_proxy Whether to bypass the HM proxy check. Defaults to false.
- * @return boolean
- */
-function is_hm_admin( $user = false, $bypass_proxy = false ) {
-	return hm_is_admin()->is_hm_admin( $user, $bypass_proxy );
-}
-
-// Kick it off.
-add_action( 'admin_init', array( hm_is_admin(), 'add_cap_if_not_exists' ) );
